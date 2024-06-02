5afb334e1af5fd85b979a567a289528578abf776
amornnan19 <amornnan19@gmail.com>
2024-06-02 18:08:57 +0700

    Create modal.blade.php

---
+++ resources/views/include/modal.blade.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="" class="img-fluid w-100" id="modalImage" alt="Modal Image">
+            </div>
+        </div>
+    </div>
+</div>
